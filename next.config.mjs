54a5b150361db9190a55d9e6d4f80a18827d21d9
nolandex <stemapal1234@gmail.com>
2025-06-11 11:48:35 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "randomuser.me"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      // Allow sitemap.xml to be served as static file from /public
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap.xml",
+      },
+      // Global fallback (misalnya ke homepage)
+      {
+        source: "/:path*",
+        destination: "/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
